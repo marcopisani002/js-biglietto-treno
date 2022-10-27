22cf636c4a19711fa09f87f8f975ce016d6e20bf
marcopisani002 <pisanimarco34@gmail.com>
2022-10-27 14:44:47 +0200

    add age range and if conditions

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const kmTreno = parseInt(prompt("Inserisca il numero di km che vuole percorrere"));
+console.log("Chilometraggio inserito:", kmTreno);
+console.log("Tipologia:", typeof kmTreno);
+if  ( isNaN( kmTreno ) ){
+    
+    alert("Dato inserito non valido: Aggiornare e Riprovare inserendo un numero valido.");
+}
+
+
+
+const age = parseInt( prompt( "Inserisca l'eta del passeggero" ) );
+console.log("età inserita:", age);
+console.log("Tipologia:", typeof age);
+if ( isNaN( age ) ) {
+  alert( "Dato inserito non valido: Aggiornare e Riprovare inserendo un numero valido." );
+}
+
+
+ const prezzo = kmTreno * 0.21;
+
+console.log("Prezzo senza sconto:", prezzo);
+console.log("Tipologia:", typeof prezzo);
+
+if ( age > 18 && age < 65) {
+    console.log( "L'utente ha un'età compresa tra i 18 e i 65 e non ha diritto agli sconti." );
+  }
+  
+  // sconto per under 18 = 20%
+  
+    if ( age < 18 ) {
+    const prezzoSale = prezzo * 0.8;
+    console.log ( "prezzo finale scontato UNDER 18:",prezzoSale );
+  }
+  
+  // sconto over 65 = 40%
+   if ( age > 65  ) {
+    const prezzoSale = prezzo * 0.6;
+    console.log( "prezzo finale scontato OVER 65:",prezzoSale );
+   }
